8a127b9ed4cefcb1cbe04af68e0d5de289447dd5
Marcelo Di Jorge <marcelobdij@gmail.com>
2023-03-22 22:08:36 -0300

    feat: add a function to the admin/register page to display a thumb of imgs to be uploaded

--- assets/js/admin-register.js
+++ assets/js/admin-register.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// DISPLAY ADDITIONAL INFO IN THE FORM {
 const displayAdditionalForm = (event) => {
     const selectedOption = event.target.value;
     const houseForm = document.querySelector('.houseForm');
@@ -20,5 +21,70 @@ const displayAdditionalForm = (event) => {
 const propertyTypeSelect = document.querySelector('.form-select');
 
 propertyTypeSelect.addEventListener('change', displayAdditionalForm);
+// -------------------------------------}
 
-  
+// CREATE THUMB FOR EACH URL{
+
+function displayURLthumb() {
+  const input = document.getElementById('imgInput');
+  const imgHelp = document.getElementById('imgHelp');
+
+  let productURLs = [];
+
+  input.addEventListener('input', (e) => {
+    const inputValue = e.target.value;
+    const lastChar = inputValue.slice(-1);
+
+    if (lastChar === ',') {
+      const url = inputValue.slice(0, -1).trim();
+
+      if (url.length > 0 && !url.endsWith(',')) {
+        productURLs.push(url);
+        input.value = '';
+
+        const imgThumbs = document.getElementById('imgThumbs') || createImgThumbsDiv(imgHelp);
+
+        const thumbnail = createThumbnail(url, productURLs);
+        imgThumbs.appendChild(thumbnail);
+      }
+    }
+  });
+}
+
+function createImgThumbsDiv(imgHelp) {
+  const imgThumbs = document.createElement('div');
+  imgThumbs.id = 'imgThumbs';
+  imgHelp.parentNode.insertBefore(imgThumbs, imgHelp.nextSibling);
+  return imgThumbs;
+}
+
+function createThumbnail(url, productURLs) {
+  const thumbnailWrapper = document.createElement('div');
+  thumbnailWrapper.classList.add('thumbnail-wrapper');
+
+  const thumbnail = document.createElement('img');
+  thumbnail.src = url;
+  thumbnail.classList.add('img-thumb');
+  thumbnailWrapper.appendChild(thumbnail);
+
+  const closeButton = document.createElement('span');
+  closeButton.classList.add('close-button');
+  closeButton.innerHTML = '&times;';
+  closeButton.addEventListener('click', () => {
+    thumbnailWrapper.remove();
+    const index = productURLs.indexOf(url);
+    if (index > -1) {
+      productURLs.splice(index, 1);
+    }
+  });
+  thumbnailWrapper.appendChild(closeButton);
+
+  return thumbnailWrapper;
+}
+
+// Call the function
+displayURLthumb();
+
+
+
+// -------------------------------------}
